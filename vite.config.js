393954ca6da55cd61c152d48bf90bda5f2aae817
mon_ami <Amit.yogev@gmail.com>
2025-11-03 15:19:09 +0200

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/DoktorABC-Mock/', // This is your GitHub repo name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
